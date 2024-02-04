03418b5e432e44a1ad4696e8184400d5fc5f2e4b
whyyou696 <whyyousick696@gmail.com>
2024-02-04 17:33:31 +0700

    feat:client-urldeploy

--- app/config/apolloClient.js
+++ app/config/apolloClient.js
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import * as SecureStore from 'expo-secure-store';
 
 const httpLink = createHttpLink({
-  uri: 'https://ed67-180-248-30-109.ngrok-free.app/',
+  uri: 'https://gc01.wahyurj.my.id/',
 });
 
 const authLink = setContext(async(_, { headers }) => {
