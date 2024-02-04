06818abdc595fc30df3fb8817b2eca8406169af9
whyyou696 <whyyousick696@gmail.com>
2024-02-04 17:28:44 +0700

    feat:serverfix

--- server/index.js
+++ server/index.js
@@ -1,28 +1,39 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const {ApolloServer} = require("@apollo/server");
-const {startStandaloneServer} = require("@apollo/server/standalone")
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 const { connect } = require("./config/mongodb");
-const authentication= require("./middlewares/authentication");
-const { typeDefs: userTypeDefs, resolvers: userResolvers } = require("./schemas/user");
-const { typeDefs: postTypeDefs, resolvers: postResolvers } = require("./schemas/post");
-const { typeDefs: followTypeDefs, resolvers: followResolvers } = require("./schemas/follow");
+const authentication = require("./middlewares/authentication");
+const {
+  typeDefs: userTypeDefs,
+  resolvers: userResolvers,
+} = require("./schemas/user");
+const {
+  typeDefs: postTypeDefs,
+  resolvers: postResolvers,
+} = require("./schemas/post");
+const {
+  typeDefs: followTypeDefs,
+  resolvers: followResolvers,
+} = require("./schemas/follow");
 
 const server = new ApolloServer({
   typeDefs: [userTypeDefs, postTypeDefs, followTypeDefs],
   resolvers: [userResolvers, postResolvers, followResolvers],
 });
 
-connect().then(() => {
-  return startStandaloneServer(server, {
-      listen: { port: 3000 || process.env.PORT },
+connect()
+  .then(() => {
+    return startStandaloneServer(server, {
+      listen: { port: process.env.PORT || 3000 },
       context: ({ req }) => {
-          return {
-              authentication: () => authentication(req),
-          }
-      }
+        return {
+          authentication: () => authentication(req),
+        };
+      },
+    });
   })
-}).then(({ url }) => {
-  console.log(`🚀  Server ready at: ${url}`);
-});
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at: ${url}`);
+  });
